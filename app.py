0189633e3d61226f8b1d118b1168c2fe6140f28f
siri1 <silvestrekassoka@gmail.com>
2022-01-02 02:01:19 +0100

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def add_numbers_post():
   	      		total += int(str_num)
   	      	return render_template('add_numbers.html', result=str(total))
   	      except ValueError:
-  	      	return render_template('add_numbers.html', ("Easy now! Let's keep it simple! 2 numbers with a space between them please"))
+  	      	return "Easy now! Let's keep it simple! 2 numbers with a space between them please"
 
 
 @app.route('/shopping_list', methods=['GET','POST'])
